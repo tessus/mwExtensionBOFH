7fa496f35b622511d64b5ad884de7b69f762c383
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2014-03-26 02:35:59 -0400

    added license info

--- BOFH.php
+++ BOFH.php
@@ -8,8 +8,8 @@
  * https://github.com/tessus/mwExtensionBOFH
  *
  * Sample template for displaying the BOFH excuse in a nostalgic look:
- * 
- * Template:BOFH 
+ *
+ * Template:BOFH
  *
  * <table border="4" width="{{{1|600}}}" cellspacing="0" cellpadding="10" bgcolor="#000000">
  * <td><font color="#00ff40">
@@ -17,6 +17,20 @@
  * </font></td>
  * </table>
  *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; if not, write to the Free Software Foundation, Inc.,
+ * 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+ * http://www.gnu.org/copyleft/gpl.html
  */
  
 if( !defined( 'MEDIAWIKI' ) ) 
