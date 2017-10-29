e882ad741122f2bfbe59a1c371dec7e9b18878e4
Helmut K. C. Tessarek <tessarek@evermeet.cx>
2017-10-28 23:23:14 -0400

    fix the extension name in old entry point

--- BOFH.php
+++ BOFH.php
@@ -2,11 +2,11 @@
 if ( function_exists( 'wfLoadExtension' ) ) {
     wfLoadExtension( 'BOFH' );
     wfWarn(
-        'Deprecated PHP entry point used for the FooBar extension. ' .
+        'Deprecated PHP entry point used for the BOFH extension. ' .
         'Please use wfLoadExtension instead, ' .
         'see https://www.mediawiki.org/wiki/Extension_registration for more details.'
     );
     return;
 } else {
-    die( 'This version of the FooBar extension requires MediaWiki 1.25+' );
+    die( 'This version of the BOFH extension requires MediaWiki 1.25+' );
 }
